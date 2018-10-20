559a4fad9a257afefe380723418c7852cd54ac37
mrsinham <julien.lefevr@gmail.com>
2018-10-20 12:57:08 +0200

    Singular + special cases (#1)
    * Singular + special cases
    
    Like 4 weeks = 1 month, 12 months = 1 year
    
    * Internalising test package
    
    For forks and PR, it is much easier to use
    
    * PR: fixes after review

--- time.go
+++ time.go
@@ -21,22 +21,28 @@ const (
 	NotYet TrID = iota
 	// JustNow is the translation ID for the "just now" text.
 	JustNow
-	// LastMinute is the translation ID for the "1 minute ago" text.
-	LastMinute
+	// Minute is the translation ID for the "1 minute ago" text.
+	Minute
 	// Minutes is the translation ID for the "%d minutes ago" text.
 	Minutes
-	// LastHour is the translation ID for the "1 hour ago" text.
-	LastHour
+	// Hour is the singular of Hours
+	Hour
 	// Hours is the translation ID for the "%d hours ago" text.
 	Hours
 	// Yesterday is the translation ID for the "yesterday" text.
 	Yesterday
 	// Days is the translation ID for the "%d days ago" text.
 	Days
+	// Week is the singular of Weeks
+	Week
 	// Weeks is the translation ID for the "`%d weeks ago" text.
 	Weeks
+	// Month is the singular of Months
+	Month
 	// Months is the translation ID for the "%d months ago" text.
 	Months
+	// Year is the singular
+	Year
 	// Years is the translation ID for the "%d years ago" text.
 	Years
 )
@@ -50,95 +56,116 @@ type Translation map[string]Terms
 // i18n is a map of translations by language code.
 var i18n = Translation{
 	"de": {
-		NotYet:     `noch nicht`,
-		JustNow:    `im Moment`,
-		LastMinute: `vor 1 Minute`,
-		Minutes:    `vor %d Minuten`,
-		LastHour:   `vor 1 Stunde`,
-		Hours:      `vor %d Stunden`,
-		Yesterday:  `gestern`,
-		Days:       `vor %d Tagen`,
-		Weeks:      `vor %d Wochen`,
-		Months:     `vor %d Monaten`,
-		Years:      `vor %d Jahren`,
+		NotYet:    `noch nicht`,
+		JustNow:   `im Moment`,
+		Minute:    `vor %d Minute`,
+		Minutes:   `vor %d Minuten`,
+		Hour:      `vor %d Stunde`,
+		Hours:     `vor %d Stunden`,
+		Yesterday: `gestern`,
+		Days:      `vor %d Tagen`,
+		Week:      `vor %d Woche`,
+		Weeks:     `vor %d Wochen`,
+		Month:     `vor %d Monat`,
+		Months:    `vor %d Monaten`,
+		Year:      `vor %d Jahr`,
+		Years:     `vor %d Jahren`,
 	},
 	"en": {
-		NotYet:     `not yet`,
-		JustNow:    `just now`,
-		LastMinute: `1 minute ago`,
-		Minutes:    `%d minutes ago`,
-		LastHour:   `1 hour ago`,
-		Hours:      `%d hours ago`,
-		Yesterday:  `yesterday`,
-		Days:       `%d days ago`,
-		Weeks:      `%d weeks ago`,
-		Months:     `%d months ago`,
-		Years:      `%d years ago`,
+		NotYet:    `not yet`,
+		JustNow:   `just now`,
+		Minute:    `%d minute ago`,
+		Minutes:   `%d minutes ago`,
+		Hour:      `%d hour ago`,
+		Hours:     `%d hours ago`,
+		Yesterday: `yesterday`,
+		Days:      `%d days ago`,
+		Week:      `%d week ago`,
+		Weeks:     `%d weeks ago`,
+		Month:     `%d month ago`,
+		Months:    `%d months ago`,
+		Year:      `%d year ago`,
+		Years:     `%d years ago`,
 	},
 	"es": {
-		NotYet:     `aún no`,
-		JustNow:    `al instante`,
-		LastMinute: `hace 1 minuto`,
-		Minutes:    `hace %d minutos`,
-		LastHour:   `hace 1 hora`,
-		Hours:      `hace %d horas`,
-		Yesterday:  `ayer`,
-		Days:       `hace %d días`,
-		Weeks:      `hace %d semanas`,
-		Months:     `hace %d meses`,
-		Years:      `hace %d años`,
+		NotYet:    `aún no`,
+		JustNow:   `al instante`,
+		Minute:    `hace %d minuto`,
+		Minutes:   `hace %d minutos`,
+		Hour:      `hace %d hora`,
+		Hours:     `hace %d horas`,
+		Yesterday: `ayer`,
+		Days:      `hace %d días`,
+		Week:      `hace %d semana`,
+		Weeks:     `hace %d semanas`,
+		Month:     `hace %d mes`,
+		Months:    `hace %d meses`,
+		Year:      `hace %d año`,
+		Years:     `hace %d años`,
 	},
 	"fr": {
-		NotYet:     `pas encore`,
-		JustNow:    `à l'instant`,
-		LastMinute: `il y a 1 minute`,
-		Minutes:    `il y a %d minutes`,
-		LastHour:   `il y a 1 heure`,
-		Hours:      `il y a %d heures`,
-		Yesterday:  `hier`,
-		Days:       `il y a %d jours`,
-		Weeks:      `il y a %d semaines`,
-		Months:     `il y a %d mois`,
-		Years:      `il y a %d ans`,
+		NotYet:    `pas encore`,
+		JustNow:   `à l'instant`,
+		Minute:    `il y a %d minute`,
+		Minutes:   `il y a %d minutes`,
+		Hour:      `il y a %d heure`,
+		Hours:     `il y a %d heures`,
+		Yesterday: `hier`,
+		Days:      `il y a %d jours`,
+		Week:      `il y a %d semaine`,
+		Weeks:     `il y a %d semaines`,
+		Month:     `il y a %d mois`,
+		Months:    `il y a %d mois`,
+		Year:      `il y a %d an`,
+		Years:     `il y a %d ans`,
 	},
 	"it": {
-		NotYet:     `non ancora`,
-		JustNow:    `al momento`,
-		LastMinute: `1 minuto fa`,
-		Minutes:    `%d minuti fa`,
-		LastHour:   `1 ora fa`,
-		Hours:      `%d ore fa`,
-		Yesterday:  `ieri`,
-		Days:       `da %d giorni`,
-		Weeks:      `da %d settimane`,
-		Months:     `da %d mesi`,
-		Years:      `da %d anni`,
+		NotYet:    `non ancora`,
+		JustNow:   `al momento`,
+		Minute:    `%d minuto fa`,
+		Minutes:   `%d minuti fa`,
+		Hour:      `%d ora fa`,
+		Hours:     `%d ore fa`,
+		Yesterday: `ieri`,
+		Days:      `da %d giorni`,
+		Week:      `da %d settimana`,
+		Weeks:     `da %d settimane`,
+		Month:     `da %d mese`,
+		Months:    `da %d mesi`,
+		Year:      `da %d anno`,
+		Years:     `da %d anni`,
 	},
 	"nl": {
-		NotYet:     `nog niet`,
-		JustNow:    `dit moment`,
-		LastMinute: `1 minuut geleden`,
-		Minutes:    `%d minuten geleden`,
-		LastHour:   `1 uur geleden`,
-		Hours:      `%d uren geleden`,
-		Yesterday:  `gisteren`,
-		Days:       `%d dagen geleden`,
-		Weeks:      `%d weken geleden`,
-		Months:     `%d maanden geleden`,
-		Years:      `%d jaar geleden.`,
+		NotYet:    `nog niet`,
+		JustNow:   `dit moment`,
+		Minute:    `%d minuut geleden`,
+		Minutes:   `%d minuten geleden`,
+		Hour:      `%d uur geleden`,
+		Hours:     `%d uren geleden`,
+		Yesterday: `gisteren`,
+		Days:      `%d dagen geleden`,
+		Week:      `%d weke geleden`,
+		Weeks:     `%d weken geleden`,
+		Month:     `%d maand geleden`,
+		Months:    `%d maanden geleden`,
+		Year:      `%d jaar geleden.`,
+		Years:     `%d jaar geleden.`,
 	},
 	"pl": {
-		NotYet:     `jeszcze nie`,
-		JustNow:    `w tej chwili`,
-		LastMinute: `1 minutę temu`,
-		Minutes:    `%d minuty temu`,
-		LastHour:   `1 godzinę temu`,
-		Hours:      `%d godziny temu`,
-		Yesterday:  `wczoraj`,
-		Days:       `%d dni temu`,
-		Weeks:      `%d tygodnie temu`,
-		Months:     `%d miesiące temu`,
-		Years:      `%d lata temu`,
+		NotYet:    `jeszcze nie`,
+		JustNow:   `w tej chwili`,
+		Minute:    `%d minutę temu`,
+		Minutes:   `%d minuty temu`,
+		Hour:      `%d godzinę temu`,
+		Hours:     `%d godziny temu`,
+		Yesterday: `wczoraj`,
+		Days:      `%d dni temu`,
+		Week:      `%d tydzień temu`,
+		Weeks:     `%d tygodnie temu`,
+		Month:     `%d miesiąc temu`,
+		Months:    `%d miesiące temu`,
+		Year:      `%d rok temu`,
+		Years:     `%d lata temu`,
 	},
 }
 
@@ -190,24 +217,31 @@ func LocalTime(t time.Time, lang string) string {
 	switch {
 	case s < 60:
 		return tr(JustNow, lang)
-	case s < 120:
-		return tr(LastMinute, lang)
 	case s < 3600:
-		return fmt.Sprintf(tr(Minutes, lang), int(diff.Minutes()))
-	case s < 7200:
-		return tr(LastHour, lang)
+		min := int(diff.Minutes())
+		return fmt.Sprintf(tr(changeIfSing(Minutes, min), lang), min)
 	case s < 86400:
-		return fmt.Sprintf(tr(Hours, lang), int(diff.Hours()))
+		hours := int(diff.Hours())
+		return fmt.Sprintf(tr(changeIfSing(Hours, hours), lang), hours)
 	case d == 1:
 		return tr(Yesterday, lang)
 	case d < 7:
-		return fmt.Sprintf(tr(Days, lang), d)
+		return fmt.Sprintf(tr(changeIfSing(Days, d), lang), d)
 	case d < 31:
-		return fmt.Sprintf(tr(Weeks, lang), int(math.Ceil(float64(d)/7)))
+		nbWeek := int(math.Ceil(float64(d) / 7))
+		if nbWeek < 4 {
+			return fmt.Sprintf(tr(changeIfSing(Weeks, nbWeek), lang), nbWeek)
+		}
+		fallthrough
 	case d < 365:
-		return fmt.Sprintf(tr(Months, lang), int(math.Ceil(float64(d)/30)))
+		nbMonth := int(math.Ceil(float64(d) / 30))
+		if nbMonth < 12 {
+			return fmt.Sprintf(tr(changeIfSing(Months, nbMonth), lang), nbMonth)
+		}
+		fallthrough
 	default:
-		return fmt.Sprintf(tr(Years, lang), int(math.Ceil(float64(d)/365)))
+		nbYear := int(math.Ceil(float64(d) / 365))
+		return fmt.Sprintf(tr(changeIfSing(Years, nbYear), lang), nbYear)
 	}
 }
 
@@ -218,4 +252,24 @@ func tr(id TrID, lang string) string {
 		ltr = i18n["en"]
 	}
 	return ltr[id]
+}
+
+func changeIfSing(id TrID, nb int) TrID {
+	if nb != 1 {
+		return id
+	}
+	switch id {
+	case Minutes:
+		return Minute
+	case Hours:
+		return Hour
+	case Months:
+		return Month
+	case Weeks:
+		return Week
+	case Years:
+		return Year
+	default:
+		return id
+	}
 }
